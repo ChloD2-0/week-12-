1e90dfb1d4a29dad408dc733db5476924f743082
ChloD2-0 <cdougherty1121@icloud.com>
2025-11-12 20:38:27 +0000

    Week 13 #5

--- week 13  csp/2 — Logical and Chained Operators.py
+++ week 13  csp/2 — Logical and Chained Operators.py
@@ -60,10 +60,16 @@ elif score < 60:
 # Practice Problems:
 
 # Write an expression that checks if a number is between 50 and 100 (inclusive).
-
+number = int(input("Enter a number: "))
+if  50 <= number <= 100:
+    print(f"The number {number} is between 50 and 100")
+else:
+    print(f"The number {number} is not between 50 and 100")
 # Write an expression that checks if a number is NOT equal to 0 and greater than 10.
-
+x = int(input("Enter a number: "))
+print(x != 0 and x > 10) 
 # Use chained comparison to check if 3 < 4 < 5.
+print(3 < 4 < 5)
 
 # Challenge: Create a password rule using logical operators:
 
